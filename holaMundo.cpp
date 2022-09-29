40f0f40cca978395a50b9cf930b58189f134925d
eliezerlopez <contacto@eliezerlopez.com>
2022-09-29 21:02:16 +0000

    Creación de ejercicio "Hola Mundo!"

---
+++ holaMundo.cpp
@@ -0,0 +1,12 @@
+// Escribe un programa que muestre por pantalla el mensaje "Hola Mundo!"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "Hola Mundo!" << endl;
+
+	return 0;
+}
