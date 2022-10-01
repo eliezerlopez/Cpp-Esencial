7d17edac532549d1b501c303704334ffff8f7319
Eliezer López <eliezer.lopezrodriguez@alum.uca.es>
2022-10-01 17:05:52 +0200

    Nuevo ejercicio: sumar dos caracteres

---
+++ sumaDosCaracteres.cpp
@@ -0,0 +1,12 @@
+// Completa el siguiente programa realizando la suma de dos caracteres
+
+int main(){
+    
+    /* 
+       Escribe
+       tu solución
+       aquí
+    */
+    
+    return 0;
+}
